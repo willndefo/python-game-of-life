ee588b88dd11e7eb7aaffbdddae588812682c447
WillN-Git <wfndefo@gmail.com>
2022-12-22 15:34:59 +0100

    Bonus: Add a button controller

---
+++ game_controller/button.py
@@ -0,0 +1,27 @@
+import pygame
+
+
+class Button:
+    def __init__(self, x: int, y: int, w: int, h, text: str, font_size: int):
+        # Set the button dimensions and position
+        self.rect = pygame.Rect(x, y, w, h)
+
+        # Initialize the font and render the text
+        self.font = pygame.font.Font(None, font_size)
+        self.text = self.font.render(text, True, (255, 255, 255))
+
+        # Initialize the color
+        self.color = (0, 0, 0)
+
+    def draw(self, screen) -> None:
+        # Draw the button rectangle
+        pygame.draw.rect(screen, self.color, self.rect)
+
+        # Draw text label
+        text_rect = self.text.get_rect(center=self.rect.center)
+        screen.blit(self.text, text_rect)
+
+    def handle_click(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                print("Hello World")
