4134e1d6b7b24f3c0f360287200256a0ad630d51
WillN-Git <wfndefo@gmail.com>
2022-12-22 11:10:03 +0100

    Issue fixed: play doesn't working because it was in the constructor

--- game_of_life.py
+++ game_of_life.py
@@ -1,6 +1,5 @@
 import pygame
 from constants import *
-import numpy as np
 
 class Game_Of_Life:
     def __init__(self):
@@ -13,34 +12,40 @@ class Game_Of_Life:
 
         # Initialize pygame
         pygame.init()
-
         self.screen: pygame.surface.Surface = pygame.display.set_mode((self.width, self.height))
-
         self.clock = pygame.time.Clock()
 
-        def play():
-            done: bool = False
+    def play(self) -> None:
+        """
+            Launch the game
+            :param self:
+            :return: None
+        """
+        done: bool = False
 
-            # While the game is not over
-            while not done:
+        # While the game is not over
+        while not done:
 
-                # state = get_next_state(state)
-                #
-                # for row in range(1, len(state) - 1):
-                #     for col in range(1, len(state) - 1):
-                #         if state[row][col] == 1:
-                #             draw_at_case(row, col, TILE_ALIVE_COLOR)
-                #         else:
-                #             draw_at_case(row, col, TILE_DEAD_COLOR)
+            # state = get_next_state(state)
+            #
+            # for row in range(1, len(state) - 1):
+            #     for col in range(1, len(state) - 1):
+            #         if state[row][col] == 1:
+            #             draw_at_case(row, col, TILE_ALIVE_COLOR)
+            #         else:
+            #             draw_at_case(row, col, TILE_DEAD_COLOR)
 
 
-                # Listen for all events
-                for event in pygame.event.get():
+            # Listen for all events
+            for event in pygame.event.get():
 
-                    # Quit the infinite loop when the user presses the close button
-                    if event.type == pygame.QUIT:
-                        done = True
+                # Quit the infinite loop when the user presses the close button
+                if event.type == pygame.QUIT:
+                    done = True
 
-                self.clock.tick(1)
+            self.clock.tick(1)
 
-            pygame.quit()
+        pygame.quit()
+
+gol = Game_Of_Life()
+gol.play()
